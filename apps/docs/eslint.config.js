23cb586fbd72e71aee2810ef77d2f9ace6a4a656
Girish Chaudhari <girish.chaudhari@visaero.com>
2026-01-21 17:59:21 +0530

    feat: initialize new docs application with its configuration and dependencies.

--- apps/docs/eslint.config.js
+++ apps/docs/eslint.config.js
@@ -1,7 +1,17 @@
 //  @ts-check
 
-import { tanstackConfig } from '@tanstack/eslint-config'
-import { restrictEnvAccess } from '@repo/eslint-config/base'
-import reactConfig from '@repo/eslint-config/react'
+import { tanstackConfig } from "@tanstack/eslint-config";
+import { restrictEnvAccess } from "@repo/eslint-config/base";
+import reactConfig from "@repo/eslint-config/react";
 
-export default [...tanstackConfig, ...restrictEnvAccess, ...reactConfig]
+export default [
+  ...tanstackConfig,
+  ...restrictEnvAccess,
+  ...reactConfig,
+  {
+    files: ["vite.config.ts"],
+    rules: {
+      "no-restricted-properties": "off",
+    },
+  },
+];
