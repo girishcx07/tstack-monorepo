415a769b936e4d5e7ce9a6926b3d33da8a78a1e2
Girish Chaudhari <girish.chaudhari@visaero.com>
2026-01-21 13:27:11 +0530

    fix: re-add apps/notebook files improperly tracked as git submodule

---
+++ apps/notebook/vite.config.ts
@@ -0,0 +1,101 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath, URL } from 'url'
+import * as v from 'valibot';
+
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+
+
+/**
+ * Fixes issue with "__dirname is not defined in ES module scope"
+ * https://flaviocopes.com/fix-dirname-not-defined-es-module-scope
+ *
+ * This is only necessary when using vite with `--configLoader runner`.
+ * We use this option to allow for importing TS files from monorepos.
+ * https://vite.dev/config/#configuring-vite
+ */
+
+const envSchema = v.object({
+  /**
+   * Since vite is only used during development, we can assume the structure
+   * will resemble a URL such as: http://localhost:3035.
+   * This will then be used to set the vite dev server's host and port.
+   */
+  PUBLIC_WEB_URL: v.pipe(
+    v.optional(v.string(), 'http://localhost:3035'),
+    v.url(),
+  ),
+
+  /**
+   * Set this if you want to run or deploy your app at a base URL. This is
+   * usually required for deploying a repository to Github/Gitlab pages.
+   */
+  PUBLIC_BASE_PATH: v.pipe(v.optional(v.string(), '/'), v.startsWith('/')),
+});
+
+const env = v.parse(envSchema, process.env);
+const webUrl = new URL(env.PUBLIC_WEB_URL);
+const host = webUrl.hostname;
+const port = parseInt(webUrl.port, 10);
+
+
+const config = defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: env.PUBLIC_BASE_PATH,
+  envPrefix: 'PUBLIC_',
+  server: {
+    host,
+    port,
+    strictPort: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        /**
+         * Modified from:
+         * https://github.com/vitejs/vite/discussions/9440#discussioncomment-11430454
+         */
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const modulePath = id.split('node_modules/')[1];
+            const topLevelFolder = modulePath?.split('/')[0];
+            if (topLevelFolder !== '.pnpm') {
+              return topLevelFolder;
+            }
+            const scopedPackageName = modulePath?.split('/')[1];
+            const chunkName =
+              scopedPackageName?.split('@')[
+              scopedPackageName.startsWith('@') ? 1 : 0
+              ];
+            return chunkName;
+          }
+        },
+      },
+    },
+  },
+  plugins: [
+    devtools(),
+    nitro(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+  ],
+})
+
+export default config
