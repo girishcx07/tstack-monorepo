6ce1af9075fe975b67f7b3aa33b988fd63e0d55e
Girish Chaudhari <girish.chaudhari@visaero.com>
2026-01-21 16:49:53 +0530

    feat: add new docs application with initial routes and configurations

---
+++ apps/docs/eslint.config.js
@@ -0,0 +1,7 @@
+//  @ts-check
+
+import { tanstackConfig } from '@tanstack/eslint-config'
+import { restrictEnvAccess } from '@repo/eslint-config/base'
+import reactConfig from '@repo/eslint-config/react'
+
+export default [...tanstackConfig, ...restrictEnvAccess, ...reactConfig]
