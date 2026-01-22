7b03543d5c7552fda1b160d3e2c3148c954f33b3
Girish Chaudhari <girish.chaudhari@visaero.com>
2026-01-22 09:36:36 +0530

    feat: Add new docs application with Vite configuration and dependencies.

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -4,15 +4,73 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
+import * as v from 'valibot';
 
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+
+
+const envSchema = v.object({
+  /**
+   * Since vite is only used during development, we can assume the structure
+   * will resemble a URL such as: http://localhost:3035.
+   * This will then be used to set the vite dev server's host and port.
+   */
+  PUBLIC_WEB_URL: v.pipe(
+    v.optional(v.string(), 'http://localhost:3035'),
+    v.url(),
+  ),
+
+  /**
+   * Set this if you want to run or deploy your app at a base URL. This is
+   * usually required for deploying a repository to Github/Gitlab pages.
+   */
+  PUBLIC_BASE_PATH: v.pipe(v.optional(v.string(), '/'), v.startsWith('/')),
+});
+
+const env = v.parse(envSchema, process.env);
+const webUrl = new URL(env.PUBLIC_WEB_URL);
+const host = webUrl.hostname;
+const port = parseInt(webUrl.port, 10);
+
 
 const config = defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  envPrefix: 'PUBLIC_',
+  base: env.PUBLIC_BASE_PATH,
+  build: {
+    rollupOptions: {
+      output: {
+        /**
+         * Modified from:
+         * https://github.com/vitejs/vite/discussions/9440#discussioncomment-11430454
+         */
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const modulePath = id.split('node_modules/')[1];
+            const topLevelFolder = modulePath?.split('/')[0];
+            if (topLevelFolder !== '.pnpm') {
+              return topLevelFolder;
+            }
+            const scopedPackageName = modulePath?.split('/')[1];
+            const chunkName =
+              scopedPackageName?.split('@')[
+                scopedPackageName.startsWith('@') ? 1 : 0
+              ];
+            return chunkName;
+          }
+        },
+      },
+    },
+  },
+  server: {
+    host,
+    port,
+    strictPort: true,
   },
   plugins: [
     devtools(),
